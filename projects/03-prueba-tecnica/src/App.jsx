26891bd362fcd284f82b24d30297eda4f054b8ee
Emanuel Urruzola <emaurruzola@gmail.com>
2023-04-16 22:17:44 -0300

    A new project with custom hooks, fetch and playwright simple test

---
+++ projects/03-prueba-tecnica/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useCatImage } from './hooks/useCatImage,js'
+import { useCatFact } from './hooks/useCatFact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&colore+red&json=true`
+
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageURL } = useCatImage({ fact })
+
+  const handleClick = async () => {
+    refreshFact()
+  }
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+
+      <button onClick={handleClick}>Get new fact
+      </button>
+      {fact && <p>{fact}</p>}
+      {imageURL && <img src={imageURL} alt={`Image extracted using the first words for ${fact}`} />}
+    </main>
+  )
+} // Devuelve el imgURL con la imagen
