971fe9ec99ef02bcc890fb6492e86972ae61f313
Emanuel Urruzola <emaurruzola@gmail.com>
2023-05-16 14:43:06 -0300

    Start with the cart context

--- projects/05-shopping-cart/src/context/Cart.jsx
+++ projects/05-shopping-cart/src/context/Cart.jsx
@@ -1,11 +1,42 @@
 import { createContext, useState } from 'react'
 
-const CartContext = React.createContext()
+export const CartContext = createContext()
 
 export function CartProvider ({ children }) {
   const [cart, setCart] = useState([])
 
   const addToCart = product => {
+    // Check if the product is already in the cart
+    const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+    if (productInCartIndex >= 0) {
+      // Forma menos eficiente
+      const newCart = structuredClone(cart)
+      newCart[productInCartIndex].quantity += 1
+      return setCart(newCart)
+    }
+
+    // Producto no esta en el carrito
+    setCart(prevState => ([
+      ...prevState,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]))
+  }
+
+  const clearCart = () => {
     setCart([])
   }
+  return (
+    <CartContext.Provider value={{
+      cart,
+      addToCart,
+      clearCart
+    }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
